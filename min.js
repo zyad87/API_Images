56bdd0b63c5f7bf81023a31235f7a1f2dc51243e
zyad87 <zyadt871@gmail.com>
2024-09-16 14:31:56 +0300

    complete

--- min.js
+++ min.js
@@ -5,42 +5,54 @@ let body = document.querySelector('body');
 let container = document.querySelector('.container');
 body.appendChild(container);
 
+
+
 async function addImage() {
-  let url = `https://66e80437b17821a9d9daf9ba.mockapi.io/Images`;
-  let nameImage = document.createElement('p');
-  let card = document.createElement('div');
-  let removeButton = document.createElement('button');
-  removeButton.classList.add('removeButton');
-  removeButton.innerText = 'Remove';
-
-  card.classList.add('card');
-
-  let imagePlaceHolder = document.createElement('img');
-  imagePlaceHolder.src = imageLink;
-  nameImage.innerText = imageName.value;
-  card.append(nameImage);
-  card.append(imagePlaceHolder);
-  card.append(removeButton);
-  container.append(card);
-
-  let res = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
-      imageName: imageName.value,
-      imageLink: imageLink.value,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  let data = await res.json();
-  console.log(data);
-  imagePlaceHolder.src = data.imageLink;
-  removeButton.setAttribute('onclick', `removeImage(${data.id})`);
-imageName.value = ""
-  ImageLink.value = ""
+    let imageNameValue = imageName.value.trim();
+    let imageLinkValue = imageLink.value.trim();
   
-}
+    if (imageNameValue === '' || imageLinkValue === '') {
+      alert('الرجاء التحقق بشكل صحيح');
+    }else{
+        let url = 'https://66e80437b17821a9d9daf9ba.mockapi.io/Images';
+        let nameImage = document.createElement('p');
+        let card = document.createElement('div');
+        let removeButton = document.createElement('button');
+        removeButton.classList.add('removeButton');
+        removeButton.innerText = 'Remove';
+      
+        card.classList.add('card');
+      
+        let imagePlaceHolder = document.createElement('img');
+        imagePlaceHolder.src = imageLinkValue;
+        nameImage.innerText = imageNameValue;
+        card.append(nameImage);
+        card.append(imagePlaceHolder);
+        card.append(removeButton);
+        container.append(card);
+      
+        let res = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify({
+            imageName: imageNameValue,
+            imageLink: imageLinkValue,
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        });
+        let data = await res.json();
+        console.log(data);
+        imagePlaceHolder.src = data.imageLink;
+        removeButton.setAttribute('onclick', `removeImage(${data.id})`);
+        
+        // إعادة تعيين الحقول
+        imageName.value = "";
+        imageLink.value = "";
+    }
+  
+
+  }
 
 async function fetchImages() {
   let url = `https://66e80437b17821a9d9daf9ba.mockapi.io/Images`;
